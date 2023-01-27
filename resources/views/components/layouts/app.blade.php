bd8bfe10824683f504b3f0e48f81baec8e1ee90d
Matt <mattsandovalb@gmail.com>
2023-01-27 11:39:29 +0100

    styles

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,11 +8,11 @@
     <meta name="description" content="{{$metaDescription ?? 'Default meta-description'}}" />
     @vite(['resources/css/app.css'])
 </head>
-<body>
+<body class="antialiased bg-slate-100 dark:bg-slate-900">
     
    <x-layouts.nav />
    @if (session('status'))
-        <div>
+        <div class="max-w-screen-xl px-3 py-2 mx-auto font-bold text-white sm:px-6 lg:px-8 bg-emerald-500 dark:bg-emerald-700">
             {{ session('status') }}
         </div>
     @endif
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,26 +1,18 @@
-<x-layouts.app title="Blog" meta-description="Blog meta description">
+<x-layouts.app
+    title="Crear nuevo post"
+    meta-description="Formulario para crear un nuevo blog post"
+>
+    <h1 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500">Create new post</h1>
 
-    <h1>Create a new Post</h1>
-
-    <form action="{{ route('posts.store', $post) }}" method="POST">
+    <form class="max-w-xl px-8 py-4 mx-auto bg-white rounded shadow dark:bg-slate-800" action="{{ route('posts.store') }}" method="POST">
         @csrf
-        <label for="">
-            Title<br>
-            <input name="title" type="text" value="{{ old('title', $post->title) }}">
-            @error('title')
-                <br>
-                <small style="color:red">{{ $message }}</small>
-            @enderror
-        </label><br>
-        <label for="">
-            Body<br>
-            <textarea name="body" cols="30" rows="10">{{ old('body', $post->body) }}</textarea>
-            @error('body')
-                <br>
-                <small style="color:red">{{ $message }}</small>
-            @enderror
-        </label><br>
-        <button type="submit">Send</button>
+
+        @include('posts.form-fields')
+
+        <div class="flex items-center justify-between mt-4">
+            <a class="text-sm font-semibold underline border-2 border-transparent rounded dark:text-slate-300 text-slate-600 focus:border-slate-500 focus:outline-none" href="{{ route('posts.index') }}">Regresar</a>
+
+            <button class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-center text-white uppercase transition duration-150 ease-in-out border border-2 border-transparent rounded-md dark:text-sky-200 bg-sky-800 hover:bg-sky-700 active:bg-sky-700 focus:outline-none focus:border-sky-500" type="submit">Enviar</button>
+        </div>
     </form>
-    <a href="{{ route('posts.index') }}">Regresar</a>
 </x-layouts.app>
